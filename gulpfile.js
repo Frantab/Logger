868d2d8ff7841bda5ef269a211aaf847d9b7e27e
frantisek.bachrony <frantisek.bachrony@firma.seznam.cz>
2018-12-06 12:43:52 +0100

    Init: Created package.json and conf files for packages.

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const eslint = require('gulp-eslint');
+const rollup = require('gulp-rollup');
+const rename = require('gulp-rename');
+const shell = require('gulp-shell');
+
+const jsDir = 'static/js/';
+const distDir = 'dist/';
+
+/**
+ * This task checks if scripts are written correctly.
+ */
+gulp.task('lint', () => {
+	return gulp.src([jsDir + '**/*.js'])
+		.pipe(eslint())
+		.pipe(eslint.format())
+		.pipe(eslint.failAfterError());
+});
+
+/**
+ * This task compile scripts into one es5 script.
+ */
+gulp.task('build', ['lint'], () => {
+	gulp.src([jsDir + '**/*.js'])
+		.pipe(rollup({
+			format: 'cjs',
+			input: jsDir + 'main.js',
+			treeshake: false
+		}))
+		.pipe(babel({
+			presets: ['env']
+		}))
+		.on('error', showError)
+		.pipe(rename('Logger.js'))
+		.pipe(gulp.dest(distDir))
+});
+
+/**
+ * This task generate documentation of all js files. Documentation is generated via esdoc.
+ */
+gulp.task('esdoc', shell.task([
+  './node_modules/.bin/esdoc'
+]));
+
+/**
+ * Error printer.
+ * @param {Error} error
+ */
+function showError (error) {
+	  console.error(error);
+	  this.emit('end');
+}
